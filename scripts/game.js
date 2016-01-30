1ce9013bee5264717b1a8f12f73f42073322a825
Radoslav Stankov <rstankov@gmail.com>
2016-01-30 20:18:18 +0200

    Add support for multiple targets

--- scripts/game.js
+++ scripts/game.js
@@ -126,7 +126,7 @@ define(function(require, exports, module) {
 
             ui = new UIManager(game);
 
-            seek(map, [cow.sprite], creepsGroup);
+            seek(map, [cow.sprite, mouse.sprite], creepsGroup);
             //inputsText = game.add.text(1, 36);
         }
 

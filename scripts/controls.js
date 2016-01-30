bb83ca4800f7d39057fbd75eb45688d5453e4659
1le <ilian.nikolov@gmail.com>
2016-01-30 10:18:34 +0200

    basic files setup - require, phaser, game, settings files

---
+++ scripts/controls.js
@@ -0,0 +1,14 @@
+define(function(require, exports, module) {
+
+    var Controls = {};
+
+    Controls.init = function(player1, player2) {
+
+    };
+
+    Controls.handle = function(keyevents) {
+
+    };
+
+    return Controls;
+});

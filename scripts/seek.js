14eb56737de318b50d88b99af6e064fa445543c3
Radoslav Stankov <rstankov@gmail.com>
2016-01-31 11:24:54 +0200

    Simplify seeking

--- scripts/seek.js
+++ scripts/seek.js
@@ -2,40 +2,20 @@ define(function(require, exports, module) {
   var EasyStar = require('easystar');
 
   var TIMEOUT_TIME = 400;
-  var ENEMY_SPEED = 90;
-
-  var DIRECTIONS = {
-    N:    {x: -1, y: -1},
-    S:    {x:  1, y:  1},
-    E:    {x:  1, y: -1},
-    W:    {x: -1, y:  1},
-    SE:   {x:  1, y:  0},
-    NW:   {x: -1, y:  0},
-    SW:   {x:  0, y:  1},
-    NE:   {x:  0, y: -1},
-    STOP: {x:  0, y:  0},
-  };
-
-  function applyDirection(object, name, speed) {
-    var direction = DIRECTIONS[name] || DIRECTIONS['STOP'];
-
-    object.body.velocity.x = direction.x * speed;
-    object.body.velocity.y = direction.y * speed;
-  }
-
-  function defaultCallback(enemy, direction) {
-    applyDirection(enemy, direction, ENEMY_SPEED);
-  }
+  var ENEMY_SPEED = 50;
 
   function random(num) {
     return Math.round(Math.random() * num);
   }
 
-  return function seek(map, targets, enemies, callback) {
-    if (!callback) {
-      callback = defaultCallback;
-    }
+  function normalize(num) {
+    if (num < -1) return -1;
+    if (num > 1) return 1;
 
+    return Math.round(num);
+  }
+
+  return function seek(map, targets, enemies, callback) {
     var easystar = new EasyStar.js();
 
     easystar.setGrid(map.tiles);
@@ -51,52 +31,25 @@ define(function(require, exports, module) {
           enemy.target = targets[random(targets.length - 1)];
         }
 
-        var currentPlayerXtile = map.tileX(enemy.target.body.position.x);
-        var currentPlayerYtile = map.tileY(enemy.target.body.position.y);
-        var currentEnemyXtile = map.tileX(enemy.body.position.x);
-        var currentEnemyYtile = map.tileY(enemy.body.position.y);
+        // var targetX = map.tileX(enemy.target.body.position.x + enemy.width);
+        var targetX = map.tileX(enemy.target.body.position.x);
+        var targetY = map.tileY(enemy.target.body.position.y + enemy.target.height);
+        // var currentX = map.tileX(enemy.body.position.x + enemy.width);
+        var currentX = map.tileX(enemy.body.position.x);
+        var currentY = map.tileY(enemy.body.position.y + enemy.height);
 
-        easystar.findPath(currentEnemyXtile, currentEnemyYtile, currentPlayerXtile, currentPlayerYtile, function(path) {
-          if (!path || !path[1]) {
-            return callback(enemy, 'STOP');
+        easystar.findPath(currentX, currentY, targetX, targetY, function(path) {
+          var nextX = 0;
+          var nextY = 0;
+
+          if (path && path[1]) {
+            nextX = path[1].x;
+            nextY = path[1].y;
           }
 
-          var currentNextPointX = path[1].x;
-          var currentNextPointY = path[1].y;
 
-          if (currentNextPointX < currentEnemyXtile && currentNextPointY < currentEnemyYtile) {
-            return callback(enemy, "NW");
-          }
-
-          if (currentNextPointX == currentEnemyXtile && currentNextPointY < currentEnemyYtile) {
-            return callback(enemy, "N");
-          }
-
-          if (currentNextPointX > currentEnemyXtile && currentNextPointY < currentEnemyYtile) {
-            return callback(enemy, "NE");
-          }
-
-          if (currentNextPointX < currentEnemyXtile && currentNextPointY == currentEnemyYtile) {
-            return callback(enemy, "W");
-          }
-
-          if (currentNextPointX > currentEnemyXtile && currentNextPointY == currentEnemyYtile) {
-            return callback(enemy, "E");
-          }
-
-          if (currentNextPointX > currentEnemyXtile && currentNextPointY > currentEnemyYtile) {
-            return callback(enemy, "SE");
-          }
-
-          if (currentNextPointX == currentEnemyXtile && currentNextPointY > currentEnemyYtile) {
-            return callback(enemy, "S");
-          }
-
-          if (currentNextPointX < currentEnemyXtile && currentNextPointY > currentEnemyYtile) {
-            return callback(enemy, "SW");
-          }
-
-          return callback(enemy, 'STOP');
+          enemy.body.velocity.x = normalize(nextX - currentX) * ENEMY_SPEED;
+          enemy.body.velocity.y = normalize(nextY - currentY) * ENEMY_SPEED;
         });
       });
 
